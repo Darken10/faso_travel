8f3d9e3c90bd7520c640bd398355e1a9344f2bb8
Darken10 <zerboinoussas4@gmail.com>
2024-09-16 08:16:52 +0000

    gestion de l'achat d'un ticket

---
+++ app/Enums/StatutTicket.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models\Ticket;
+
+enum StatutTicket: string
+{
+    case Payer = 'Payer';
+    case EnAttente = 'En attente';
+    case Annuler = 'Annuler';
+    case Refuser = 'Refuser';
+    case Valider = 'Valider';
+    case Pause = 'Pause';
+    case Bloquer = 'Bloquer';
+    case Suspendre = 'Suspendre';
+
+
+    public static function values():array
+    {
+        return array_column(self::cases(), 'value');
+    }
+}
